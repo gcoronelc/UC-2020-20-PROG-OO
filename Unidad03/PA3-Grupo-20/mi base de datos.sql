a001f4c5429dd5079b3e748c528e3e344be6cb75
Gustavo Coronel <gcoronelc@gmail.com>
2021-09-28 19:52:25 -0500

    Unidad 04

---
+++ Unidad03/PA3-Grupo-20/mi base de datos.sql
@@ -0,0 +1,12 @@
+
+create user grupo20 identified by admin;
+
+grant conn
+
+connect grupo20/admin;
+
+
+-- tabla cuenta
+create table cuenta(
+idcuenta 
+
---
+++ Unidad04/CargarFormulario.java
@@ -0,0 +1,35 @@
+﻿/*
+Método para cargar un formulario
+*/
+
+	private void cargarFormulario(Class<?> aClass) {
+    try {
+      // Buscar una instancia
+      JInternalFrame view = null;
+      for (JInternalFrame frame : desktopPane.getAllFrames()) {
+        if (aClass.isInstance(frame)) {
+          view = frame;
+          break;
+        }
+      }
+      // Crear la instancia
+      if (view == null) {
+        view = (JInternalFrame) Class.forName(aClass.getName()).newInstance();
+        desktopPane.add(view);
+      }
+      // Hacerlo visible
+      view.setVisible(true);
+      view.setSelected(true);
+      view.setMaximum(true);
+    } catch (Exception e) {
+    }
+
+  }
+  
+  
+/*
+La carga del formulario se hace de esta manera.
+*/  
+
+cargarFormulario(ProcDepositoView.class);
+  
